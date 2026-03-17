59305d63c0fd5a14cfb0e159ec3cfdc241730a81
DankerAlphid <tom.eggert@cptm-eggert.de>
2026-03-17 15:24:44 +0100

    additional competition skeleton for callbacks and figures

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -1,8 +1,10 @@
 from .oil_impact_callbacks import register_oil_callbacks
 from .station_callbacks import register_station_callbacks
+from .competition_callbacks import register_competition_callbacks
 
 
 def register_callbacks(app):
 
     register_oil_callbacks(app)
     register_station_callbacks(app)
+    register_competition_callbacks(app)
---
+++ src/callbacks/competition_callbacks.py
@@ -0,0 +1,5 @@
+from dash import Input, Output
+from src.figures.competition_figures import placeholder
+
+def register_competition_callbacks(app):
+    pass
