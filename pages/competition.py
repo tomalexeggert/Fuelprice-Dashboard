dd9e4ff848da76d799c7d584ec3c752895cb5ac3
Bjarne <bjarne.j.bender@gmail.com>
2026-03-18 11:31:17 +0100

    first structure other effects

--- pages/competition.py
+++ pages/competition.py
@@ -8,7 +8,45 @@ layout = dbc.Container([
     html.Br(),
     dbc.Row([
         dbc.Col([
-            html.H1("dfgdfgn")
+            html.H1("Other Effects on Fuel Prices", className="text-center"),
         ])
-    ])
+    ]),
+        dbc.Row([
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H4("Other Effects that affect the fuel prices")),
+                dbc.CardBody([
+                    html.P(
+                        "-Highway Gas Stations \n -Proximity to other stations \n -Regional based Differences \n -Extreme Weather",
+                        style={"whiteSpace": "pre-line"})
+                ])
+            ]),
+            width=12
+        )
+    ]),
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H3("The increasing Price differences of Highway, to normal Stations", className="text-center"),
+        ])
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H3("The effect of proximity to other stations on the average price", className="text-center"),
+        ])
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H3("Regional based Differences", className="text-center"),
+        ])
+    ]),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H3("Extreme Weather conditions", className="text-center"),
+        ])
+    ]),
 ])
